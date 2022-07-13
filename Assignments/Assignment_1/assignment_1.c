6188da0dd97f8d81554f802179a44e4728963d60
Aman Kumar <107614318+WebWithAman@users.noreply.github.com>
2022-07-13 09:52:05 +0530

    assignment_1 uploaded

---
+++ Assignments/Assignment_1/assignment_1.c
@@ -0,0 +1,99 @@
+// C++ Bootcamp Assignment-1 Solved All Questions Here :-
+// I have Solved All Questions in this Single file :-
+
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+
+// Main Function Start
+int main()
+{
+    
+     
+
+    // 1. Write A Program to Print  Hello Students On Screen :-
+    // printf("Hello Students");
+
+
+
+    // 2. Write A Program to Print  Hello in First Line and  Students in Second Line :-
+    // printf("Hello\nStudents");
+
+
+
+    // 3. Print "MySirG" On the Screen :-
+    // printf("\"MySirG\"");
+
+
+
+    // 4. WAP to Find Area of Circle Take Radius From User as Input :-
+    // float radius,area;
+    // printf("Enter Radius of Circle:");
+    // scanf("%f",&radius);
+    // area = 3.14 * radius * radius ;
+    // printf("\"Area of Circle is %.3f having the Radius %.3f\"",area,radius);
+
+
+
+    // 5. WAP to Calculate the  Length of String Using Printf Function :-
+    // int length;
+    // length = printf("Saurabh shukla Sir is the Best Teacher\n");
+    // printf("Length of String is :%d", length);
+
+
+    // 6. WAP to Print Name of the User in Double Quotes :-
+    // printf("\"Hello, Aman Kumar\"");
+
+
+
+    // 7. WAP to Print "%d" on the Screen :-
+    // printf("%%d");
+
+
+
+    // 8. WAP to Print "\n" on the Screen :-
+    // printf("\\n");
+
+
+
+    // 9. WAP to Print "\\" on the Screen :-
+    // printf("\\\\");
+
+
+
+    // 10. WAP to Take Date as Input in Given Format and Convert and Display in Given Format :-
+    // int date,month,year;
+    // printf("Enter Date in this Format:- DD/MM/YYYY\n");
+    // scanf("%d/%d/%d",&date,&month,&year);
+    // printf("Converted Date :-\n");
+    // printf("Day - %d , Month - %d , Year - %d",date,month,year);
+
+
+
+    // 11. WAP to Take Time as Input in Given Format and Convert and Display in Given Format :-
+    int hour,minute;
+    printf("Enter Time in this Format:- HH:MM\n");
+    scanf("%d:%d",&hour,&minute);
+    printf("Converted Time :-\n");
+    printf("%d Hour and %d  Minutes",hour,minute);
+
+
+
+    // 12. Find Output of Below Code :-
+    /*
+        int main(){
+            int x =printf("ineuron");
+            printf("%d",x);
+            return 0;
+        }
+    */
+
+    // Output of Above Code is :ineuron7
+    // 7 is the Lenght of the string "ineuron"
+
+
+
+}
+// Main Function End
