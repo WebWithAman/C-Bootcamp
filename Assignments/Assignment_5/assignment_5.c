ec0244e6da57779f6d76fd452e76491a0196a500
Aman Kumar <107614318+WebWithAman@users.noreply.github.com>
2022-07-20 22:55:10 +0530

    Assignment_5 Uploaded

---
+++ Assignments/Assignment_5/assignment_5.c
@@ -0,0 +1,115 @@
+// // C++ Bootcamp Assignment-5 Solved All Questions Here :-
+// I Solved All Questions in this Single file :-
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+
+
+
+    // 1 .WAP to Print MySirG N Times On the Screen :-
+    // int n;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // for (; n; n--)
+    //     printf("MySirG\n");
+
+
+
+    // 2. WAP to Print First N Natural  Numbers :-
+    // int i, n;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // printf("First %d Natural Numbers Are :-\n", n);
+    // for (i = 1; i <= n; i++)
+    //     printf("%d ", i);
+
+
+
+    // 3. WAP to Print First N Natural Numbers In Reverse Order :-
+    // int n;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // printf("First %d Natural Numbers In Reverse Order Are :-\n", n);
+    // for (; n; n--)
+    //     printf("%d ", n);
+
+
+
+    // 4. WAP to Print First N Odd Numbers :-
+    // int i, n;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // printf("First %d Odd Natural Numbers Are :-\n", n);
+    // for (i = 1; i <= n; i++)
+    //     printf("%d ", i * 2 - 1);
+
+
+
+    // 5. WAP to Print First N Odd Numbers In Reverse Order :-
+    // int n;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // printf("First %d Odd Natural Numbers In Reverse Order Are :-\n", n);
+    // for (; n; n--)
+    //     printf("%d ", n * 2 - 1);
+
+
+
+    // 6. WAP to Print First N Even Natural Numbers :-
+    // int i, n;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // printf("First %d Even Natural Numbers Are :-\n", n);
+    // for (i = 1; i <= n; i++)
+    //     printf("%d ", i * 2);
+
+
+
+    // 7. WAP to Print First N Even Natural Numbers In Reverse Order :-
+    // int n;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // printf("First %d Even Natural Numbers In Reverse Order Are :-\n", n);
+    // for (; n; n--)
+    //     printf("%d ", n * 2);
+
+
+
+
+    // 8. WAP to Print Squares of First N Natural Numbers :-
+    // int i, n;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // printf("Squares of First %d Natural Numbers Are :-\n", n);
+    // for (i = 1; i <= n; i++)
+    //     printf("%d ", i * i);
+
+
+
+    // 9. WAP to Print Cubes of First N Natural Numbers :-
+    // int i, n;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // printf("Cubes of First %d Natural Numbers Are :-\n", n);
+    // for (i = 1; i <= n; i++)
+    //     printf("%d ", i * i * i);
+
+
+
+    // 10. WAP to Print A Table of N :-
+    // int i, n;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // printf("Table of %d is :-\n", n);
+    // for (i = 1; i <= 10; i++)
+    //     printf("%d * %d = %d\n", n, i, n * i);
+
+    getch();
+    return 0;
+}
+// Main Function End
