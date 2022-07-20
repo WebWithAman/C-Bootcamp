05598008386d75b68e51fdf9e006955b2e53dab1
Aman Kumar <107614318+WebWithAman@users.noreply.github.com>
2022-07-20 22:39:05 +0530

    Assignment_4 Uploaded

---
+++ Assignments/Assignment_4/assignment_4.c
@@ -0,0 +1,86 @@
+// // C++ Bootcamp Assignment-4 Solved All Questions Here :-
+// I Solved All Questions in this Single file :-
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+
+    
+
+    // 1.WAP to Print MySirG 5 Time On the Screen :-
+    // for (int i = 1; i <= 5; i++)
+    //     printf("MySirG\n");
+
+
+
+    // 2. WAP to Print First 10 Natural Numbers :-
+    // printf("First 10 Natural Numbers Are :-\n");
+    // for (int i = 1; i <= 10; i++)
+    //     printf("%d ", i);
+
+
+
+    // 3. WAP to Print First 10 Natural Numbers In Reverse Order :-
+    // printf("First 10 Natural Numbers In Reverse Order Are :-\n");
+    // for (int i = 10; i >= 1; i--)
+    //     printf("%d ", i);
+
+
+
+    // 4. WAP to Print First 10 Odd Natural Numbers :-
+    // printf("First 10 Odd Natural Numbers Are :-\n");
+    // for (int i = 1; i <= 10; i++)
+    //     printf("%d ", i * 2 - 1);
+
+
+
+    // 5. WAP to Print First 10 Odd Natural Numbers In Reverse Order :-
+    // printf("First 10 Odd Natural Numbers In Reverse Order Are :-\n");
+    // for (int i = 10; i >= 1; i--)
+    //     printf("%d ", i * 2 - 1);
+
+
+
+    // 6. WAP to Print First 10 Even Natural Numbers :-
+    // printf("First 10 Even Natural Numbers Are :-\n");
+    // for (int i = 1; i <= 10; i++)
+    //     printf("%d ", i * 2);
+
+
+
+    // 7. WAP to Print First 10 Even Natural Numbers In Reverse Order :-
+    // printf("First 10 Even Natural Numbers in Reverse Order Are :-\n");
+    // for (int i = 10; i >= 1; i--)
+    //     printf("%d ", i * 2);
+
+
+
+    // 8. WAP to Print Squares of First 10 Natural Numbers :-
+    // printf("Squares of First 10 Natural Numbers Are :-\n");
+    // for (int i = 1; i <= 10; i++)
+    //     printf("%d ", i * i);
+
+
+
+    // 9. WAP to Print Cubes of First 10 Natural Numbers :-
+    // printf("Cubes of First 10 Natural Numbers Are :-\n");
+    // for (int i = 1; i <= 10; i++)
+    //     printf("%d ", i * i * i);
+
+
+
+    // 10. WAP to Print A Table of 5 :-
+    // printf("Table of 5 is :-\n");
+    // for (int i = 1; i <= 10; i++)
+    //     printf("%d * %d = %d\n", 5, i, 5 * i);
+
+
+
+    getch();
+    return 0;
+}
+// Main Function End
