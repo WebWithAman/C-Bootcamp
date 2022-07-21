0eb9733719e924eb8ae0239a1a1674f271aa5cbd
Aman Kumar <107614318+WebWithAman@users.noreply.github.com>
2022-07-21 17:01:23 +0530

    Assignment_6 Uploaded

---
+++ Assignments/Assignment_6/assignment_6.c
@@ -0,0 +1,121 @@
+// // C++ Bootcamp Assignment-6 Solved All Questions Here :-
+// I Solved All Questions in this Single file :-
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+
+
+
+    // 1. WAP to Calculate Sum of First N Natural Numbers :-
+    // int  n, sum = 0;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // for (; n; n--)
+    //     sum += n;
+    // printf("Sum of First %d Natural Numbers is : %d", n, sum);
+
+
+
+    // 2. WAP to Calculate Sum of First N Even Natural Numbers :-
+    // int i,n, sum = 0;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // for (i = n ; n; n--)
+    //     sum += n * 2;
+    // printf("Sum of First %d Even Natural Numbers is %d",i,sum);
+
+
+
+    // 3. WAP to Calculate Sum of First N Odd Natural Numbers :-
+    // int i, n, sum = 0;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // for (i = n; n; n--)
+    //     sum += n * 2 - 1;
+    // printf("Sum of First %d Odd Natural Numbers is %d", i, sum);
+
+
+
+    // 4. WAP to Print Sum of Squares of First N Natural Numbers :-
+    // int i, n, sum = 0;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // for (i = n; n; n--)
+    //     sum += n * n;
+    // printf("Sum of Squares of First %d Natural Numbers is %d", i, sum);
+
+
+
+
+    // 5. WAP to Calculate Sum of Cubes of First N Natural Numbers :-
+    // int i, n, sum = 0;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // for (i = n; n; n--)
+    //     sum += n * n * n;
+    // printf("Sum of Cubes of First %d Natural Numbers is %d", i, sum);
+
+
+
+    // 6. WAP to Calculate Factorial of A Number :-
+    // int n, i = 2, fact = 1;
+    // printf("Enter A Number:");
+    // scanf("%d", &n);
+    // fact = n == 0 || n == 1 ? 1 : fact;
+    // for (i = 2; i <= n; i++)
+    //     fact *= i;
+    // printf("Factorial of %d is %d", n, fact);
+
+
+
+    // 7. WAP to Count Digits In a Given Number :-
+    // int num, count = 0;
+    // printf("Enter A Number:");
+    // scanf("%d", &num);
+    // for (; num /= 10; count++);
+    // printf("Total Digits in Entered Number is %d", ++count);
+
+
+
+    // 8. WAP to A Given Number is Prime or Not :-
+    // int num, i;
+    // printf("Enter A Number:");
+    // scanf("%d", &num);
+    // for (i = 2; i <= num / 2; i++)
+    //     if (!(num % i))
+    //         break;
+    // i > num / 2 ? printf("%d is a Prime Number", num) : printf("%d is Not a Prime Number", num);
+
+
+
+    // 9. WAP to Calculate LCM of Two Numbers :-
+    // int num1, num2, i;
+    // printf("Enter Two Numbers :");
+    // scanf("%d%d", &num1, &num2);
+    // for (i = num1 > num2 ? num1 : num2; i <= num1 * num2; i += i)
+    //     if (!(i % num1) && !(i % num2))
+    //         break;
+    // printf("LCM of %d and %d is %d", num1, num2, i);
+
+
+
+    // 10. WAP to Reverse A Given Number :-
+    // int num, rev = 0;
+    // printf("Enter A Number:");
+    // scanf("%d", &num);
+    // while (num > 0)
+    // {
+    //     rev = rev * 10 + num % 10;
+    //     num /= 10;
+    // }
+    // printf("Reverse of Entered Number is %d",rev);
+
+    getch();
+    return 0;
+}
+// Main Function End
